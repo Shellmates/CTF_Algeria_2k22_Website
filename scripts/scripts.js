61c93747c6326ab22397ba38bc9f5a9796008ed4
grosvenor <abdo7dady@gmail.com>
2022-04-06 14:45:53 +0100

    adding faq section

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+function answer1(){
+    document.getElementById("ans1").innerHTML="shellmates"
+ }
+ function answer2(){
+     document.getElementById("ans2").innerHTML="yes"
+ }
+ function answer3(){
+     document.getElementById("ans3").innerHTML="idk"
+ }
